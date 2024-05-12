7637228651c2063da2e9ce88c6c900b598636769
OE-LUCIFER <abhat8283@gmail.com>
2024-05-12 11:11:22 +0530

    Added 2 new provider

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="ai4free",
-    version="0.5", 
+    version="0.6", 
     description="collection of free AI provides",
     long_description=README,
     long_description_content_type="text/markdown",
--- t2.py
+++
@@ -1,8 +0,0 @@
-# Import the generate function from liaobots.py
-from t import generate
-
-# Use the generate function
-response = generate("What is the weather like today?", system_prompt="Please provide a detailed forecast.")
-
-# Print the response
-print(response)
